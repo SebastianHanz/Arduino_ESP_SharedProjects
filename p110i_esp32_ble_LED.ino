a6861bb7b5088eee9b3dde0044cb7c7fb9dbf9a2
SebastianHanz <sebastianhanz@gmx.de>
2021-01-22 20:01:59 +0100

    First commit with working sketch

---
+++ p110i_esp32_ble_LED.ino
@@ -0,0 +1,80 @@
+#include <Adafruit_NeoPixel.h>
+#define PIN         19
+#define NUMPIXELS   60
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <Arduino.h>
+
+String valor, led, red, green, blue, refreshLED;
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string value = pCharacteristic->getValue();
+      if (value.length() > 0) {
+        valor = "";
+        for (int i = 0; i < value.length(); i++) {
+          valor = valor + value[i];
+        }
+
+        int einzelneVariablen[10];
+        sscanf(valor.c_str(), "%d,%d,%d,%d,%d", &einzelneVariablen[0], &einzelneVariablen[1], &einzelneVariablen[2], &einzelneVariablen[3], &einzelneVariablen[4]);
+
+        led = einzelneVariablen[0];
+        red = einzelneVariablen[1];
+        green = einzelneVariablen[2];
+        blue = einzelneVariablen[3];
+        refreshLED = einzelneVariablen[4];
+
+        if (refreshLED == 1) {
+          for (int i = 0; i < NUMPIXELS; i++) {
+            pixels.Color(i, 0, 0, 0);
+          }
+        }
+
+        Serial.print("LED = ");Serial.println(led);
+        Serial.print("red = ");Serial.println(red);
+        Serial.print("green = ");Serial.println(green);
+        Serial.print("blue = ");Serial.println(blue);
+        Serial.println();
+        
+        pixels.setPixelColor((led.toInt() - 1), pixels.Color(red.toInt(), green.toInt(), blue.toInt()));
+        pixels.show();
+
+        pCharacteristic->setValue(valor.c_str()); 
+      }
+    }
+};
+
+void setup() {
+  pixels.begin();
+  Serial.begin(115200);
+
+  BLEDevice::init("MyESP32");
+  BLEServer *pServer = BLEDevice::createServer();
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  pCharacteristic->setCallbacks(new MyCallbacks());
+  pCharacteristic->setValue("Iniciado.");
+  pService->start();
+
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  pAdvertising->start();
+}
+
+void loop() {
+  //
+}
